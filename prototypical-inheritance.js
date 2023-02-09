657abc7b4055441ed39de113565df0d5c08f539e
Fernando <fernandostarlin1012@gmail.com>
2023-02-09 14:33:11 -0400

    Whenever you change the prototype, change the constructor as well

--- prototypical-inheritance.js
+++ prototypical-inheritance.js
@@ -9,6 +9,8 @@ function Circle(radius) {
 }
 
 Circle.prototype= Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
 
 Circle.prototype.draw = function () {
     console.log('draw');

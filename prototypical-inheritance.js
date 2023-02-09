94703a124c5681c1163e4c760b8113b55a926961
Fernando <fernandostarlin1012@gmail.com>
2023-02-09 14:52:40 -0400

    Intermediate function inheritance

--- prototypical-inheritance.js
+++ prototypical-inheritance.js
@@ -5,18 +5,29 @@ Shape.prototype.duplicate = function () {
     console.log('duplicate');
 }
 
+function extend(Child, Parent) {
+    Child.prototype= Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
 function Circle(radius, color) {
     Shape.call(this, color);
     this.radius = radius;
 }
 
-Circle.prototype= Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
-
+extend(Circle, Shape);
 
 Circle.prototype.draw = function () {
     console.log('draw');
 }
+
+
+
+function Square(area) {
+    this.area = area;
+}
+
+extend(Square, Shape);
 
 const s = new Shape();
 const c = new Circle(1, 'red');
